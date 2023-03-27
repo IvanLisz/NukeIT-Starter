2533a4985a4dd8d612e2c03760ef041ce750d12b
Ivan Liszewski <ivan@lisz.org>
2023-03-27 03:17:24 -0300

    feat: Add react-dev-inspector

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
+import { Inspector, type InspectParams } from 'react-dev-inspector'
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Inspector
+        // props see docs:
+        // https://github.com/zthxxx/react-dev-inspector#inspector-component-props
+        keys={[]}
+        disableLaunchEditor={false}
+
+        onClickElement={({ codeInfo }: InspectParams) => {
+          if (!codeInfo?.absolutePath) return
+          const { absolutePath } = codeInfo
+          if (window) {
+            window.top?.postMessage("openFile", absolutePath);
+          }
+        }}
+      />
+    </>)
 };
 
 export default api.withTRPC(MyApp);
