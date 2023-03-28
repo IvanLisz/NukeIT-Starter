c7f7652bb74f4435d67761ffc142879421f0d766
Ivan Liszewski <ivan@lisz.org>
2023-03-28 00:49:54 -0300

    chore: Update react-inspector

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,20 @@
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
-import { Inspector, type InspectParams } from 'react-dev-inspector'
+import { Inspector, type InspectParams } from 'nukeit-react-inspector';
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 
-  if (typeof window !== 'undefined') {
-    window.onmessage = function (e) {
-      if (e.data == 'toggleInspector') {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
-        (window as Record<string, any>).__REACT_DEV_INSPECTOR_TOGGLE__();
-      }
-    };
-  }
-
   return (
     <>
       <Component {...pageProps} />;
       <Inspector
-        keys={[]}
-        disableLaunchEditor={true}
         onClickElement={({ codeInfo }: InspectParams) => {
-          if (!codeInfo?.absolutePath) return
+          if (!codeInfo?.absolutePath) return;
           const { absolutePath } = codeInfo
+          console.log(codeInfo);
           if (window) {
             window.top?.postMessage({ type: "openFile", value: absolutePath }, "*");
           }
